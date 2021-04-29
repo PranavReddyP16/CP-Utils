d3bd912ce96f6a6133594f52fdbc9a09ecd03ab0
P_Pranav_Reddy <bestpranav.16@gmail.com>
2021-04-29 16:13:36 +0530

    added building roads in graph algos

--- CSES/GraphAlgorithms/building_roads.cpp
+++ CSES/GraphAlgorithms/building_roads.cpp
@@ -20,7 +20,26 @@ high_resolution_clock::time_point curTime() { return high_resolution_clock::now(
 
 #define rep(i,n) for(int i=0;i<n;i++)
 
+vector<int> parent;
+vector<int> siz;
 
+int root(int u)
+{
+    while(parent[u]!=u) u = parent[u];
+    return u;
+}
+
+void merge(int u, int v)
+{
+    u = root(u);
+    v = root(v);
+    if(u==v) return;
+
+    if(siz[u]<siz[v]) swap(u,v);
+
+    parent[v] = u;
+    siz[u] += siz[v];
+}
 
 signed main()
 {
@@ -28,7 +47,33 @@ signed main()
     cin.tie(NULL);
     auto startTime = curTime();
 
-    
+    int n,m;
+    cin>>n>>m;
+
+    parent.resize(n);
+    siz.resize(n,1);
+    for(int i=0;i<n;i++) parent[i] = i;
+
+    for(int i=0;i<m;i++)
+    {
+        int u,v;
+        cin>>u>>v;
+        u--;v--;
+
+        merge(u,v);
+    }
+
+    set<int> nodes;
+    for(int i=0;i<n;i++) nodes.insert(root(i));
+
+    vector<int> ans;
+    for(int x : nodes) ans.push_back(x+1);
+
+    cout<<sz(ans)-1<<endl;
+    for(int i=0;i<sz(ans)-1;i++)
+    {
+        cout<<ans[i]<<" "<<ans[i+1]<<endl;
+    }
 
     auto stopTime = curTime();
     auto duration = duration_cast<microseconds>(stopTime - startTime);
