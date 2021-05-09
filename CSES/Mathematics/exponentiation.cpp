e656afac5a5b9f43e84e780c4349da8d672e5456
P_Pranav_Reddy <bestpranav.16@gmail.com>
2021-05-09 19:53:55 +0530

    Added a cses math folder

---
+++ CSES/Mathematics/exponentiation.cpp
@@ -0,0 +1,56 @@
+#include "bits/stdc++.h"
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+using namespace std::chrono;
+
+#define ll long long
+#define ld long double
+#define sz(c) ((ll)c.size())
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define int ll
+const ll inf = 1e18;
+
+const int mod = 1e9 + 7;
+const int mod2 = 998244353;
+
+high_resolution_clock::time_point curTime() { return high_resolution_clock::now(); }
+
+#define rep(i,n) for(int i=0;i<n;i++)
+
+int power(int a, int b)
+{
+    int ans=1;
+    while(b)
+    {
+        if(b%2) ans = (ans*a)%mod;
+        a = (a*a)%mod;
+        b>>=1;
+    }
+
+    return ans;
+}
+
+signed main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    auto startTime = curTime();
+
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    {
+        int a,b;
+        cin>>a>>b;
+
+        cout<<power(a,b)<<endl;
+    }
+
+    auto stopTime = curTime();
+    auto duration = duration_cast<microseconds>(stopTime - startTime);
+    //cout<<"Program ran for "<<(ld)duration.count()/1e6<<" "<<"seconds"<<endl;
+}
